2e6dade5069599301d98835fecf18f9ba547db17
ahafez <ahafez@tafeel.com>
2022-03-23 15:22:22 +0200

    Adding DbContext, Database Connection, GetProducts APIs

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var Products = await _context.Products.ToListAsync();
+
+            return Ok(Products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+
+    }
+}
