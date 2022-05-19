940a66e1ae54aa9c2f195f96acc45d0b303a86b9
Ahmed Hafez <ahmed.abdelghany.hafez@gmail.com>
2022-05-19 05:29:58 +0200

    Order Classes

---
+++ Core/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.OrderAggregate
+{
+    public class Order : BaseEntity
+    {
+        public Order()
+        {
+        }
+
+        public Order(IReadOnlyList<OrderItem> orderItems, string buyerEmail, Address shipToAddress, Delivery delivery, decimal subTotal)
+        {
+            OrderItems = orderItems;
+            BuyerEmail = buyerEmail;
+            ShipToAddress = shipToAddress;
+            Delivery = delivery;
+            SubTotal = subTotal;
+        }
+
+        public IReadOnlyList<OrderItem>? OrderItems { get; set; }
+
+        public string? BuyerEmail { get; set; }
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+        public Address? ShipToAddress { get; set; }
+        public Delivery? Delivery { get; set; }
+        public decimal SubTotal { get; set; }
+
+        public OrderStatus OrderStatus { get; set; } = OrderStatus.Pending;
+
+        public string? PaymentIntentId { get; set; }
+
+        public decimal GetTotal()
+        {
+            return SubTotal + Delivery.Price;
+        }
+    }
+}
---
+++ Core/Entities/OrderAggregate/OrderItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.OrderAggregate
+{
+    public class OrderItem : BaseEntity
+    {
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(ProductItemOrdered? productOrdered, decimal price, int quantity)
+        {
+            ProductOrdered = productOrdered;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public ProductItemOrdered? ProductOrdered { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
